9440768f3e8799144917130810bf9b481fa5e2f3
Technion <technion@lolware.net>
2014-08-16 09:03:26 +0000

    Challenge 15 completed.

---
+++ set2/chal15/chal15.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+#technion@lolware.net
+
+def check_pad(str)
+    last = str[-1,1]
+    
+    padstr = last.chr * last.ord
+
+    unless str.match(/#{padstr}/)
+        raise "Invalid padding"
+    end
+
+end
+
+check_pad("ICE ICE BABY\x04\x04\x04\x04")
+puts "Successful first run"
+
+begin
+    check_pad("ICE ICE BABY\x04\x04\x04\x05") 
+rescue StandardError
+    puts "Corectly raised an error"
+end
