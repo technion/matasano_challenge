8218d40e1b4c29eef902268c80d2b67a67a5681f
Technion <technion@lolware.net>
2014-08-26 08:42:37 +0000

    Completed client/server SRP implementation

--- set5/chal36/chal36.rb
+++ set5/chal36/chal36.rb
@@ -13,35 +13,88 @@ N = ("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024" +
     "bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff" +
     "fffffffffffff").to_i(16)
 
-g = 2
-k = 3
-i = "myemail"
-p = "password"
+class SRPServer
+    def initialize
+        #These items are pre-agreed on
+        @i = "myemail"
+        @p = "password"
+        @g = 2
+        @k = 3
+    end
 
+    def stepone
+        @salt = rand(1000) 
+        xh = OpenSSL::Digest::SHA256.digest(@salt.to_s + @p).unpack("H*").join
+        x = xh.to_i(16)
+        @v = @g.to_bn.mod_exp(x, N)
+        return @salt
+    end
 
-###SERVER
-salt = 435
-xh = OpenSSL::Digest::SHA256.digest(salt.to_s + p).unpack("H*").join
-x = xh.to_i(16)
-v = g.to_bn.mod_exp(x, N)
+    def getbpub
+        @b = rand(1000)
+        @bpub = @k * @v + @g.to_bn.mod_exp(@b, N)
+    end
+ 
+    def setkey(apub)
+        uh = OpenSSL::Digest::SHA256.digest(apub.to_s + @bpub.to_s).unpack("H*").join
+        u = uh.to_i(16)
+        ss = (apub * @v.mod_exp(u, N)).to_bn.mod_exp(@b, N)
+        @k = OpenSSL::Digest::SHA256.digest(ss.to_s).unpack("H*").join
+    end
 
-##CLIENT
-a = 7
-apub = g.to_bn.mod_exp(a, N)
+    def checklogin(clientkey)
+        digest = OpenSSL::Digest.new('sha256')
+        h = OpenSSL::HMAC.hexdigest(digest, @k, @salt.to_s)
+        return "OK" if h == clientkey
+        return "ERROR"
+    end
+end
 
-##SERVER
-b = 12
-bpub = k * v + g.to_bn.mod_exp(b, N)
+class SRPClient
+    def initialize(salt)
+        #Yes these are declared again. After all, the client shouldn't know it
+        #just because the server does
+        @i = "myemail"
+        @p = "password"
+        @g = 2
+        @k = 3
+        @salt = salt
+    end
 
-#BOTH
-uh = OpenSSL::Digest::SHA256.digest(apub.to_s + bpub.to_s).unpack("H*").join
-u = uh.to_i(16)
+    def getapub
+        @a = rand(1000)
+        @apub = @g.to_bn.mod_exp(@a, N)
+        return @apub
+    end
 
-##CLIENT
-sc = (bpub - k * (g.to_bn.mod_exp(x, N))).to_bn.mod_exp(a + u*x, N)
-puts "Client thinks key is #{sc}"
+    def setkey(bpub)
+        uh = OpenSSL::Digest::SHA256.digest(@apub.to_s + bpub.to_s).unpack("H*").join
+        u = uh.to_i(16)
+        xh = OpenSSL::Digest::SHA256.digest(@salt.to_s + @p).unpack("H*").join
+        x = xh.to_i(16)
+        sc = (bpub - @k * (@g.to_bn.mod_exp(x, N))).to_bn.mod_exp(@a + u*x, N)
+        @k = OpenSSL::Digest::SHA256.digest(sc.to_s).unpack("H*").join
 
-##SERVER
-ss = (apub * v.mod_exp(u, N)).to_bn.mod_exp(b, N)
-puts "Server thinks key is #{ss}"
+    end
+
+    def gethmac
+        digest = OpenSSL::Digest.new('sha256')
+        h = OpenSSL::HMAC.hexdigest(digest, @k, @salt.to_s)
+        return h
+    end
+end
+
+server = SRPServer.new
+salt = server.stepone
+
+client = SRPClient.new(salt)
+apub = client.getapub
+bpub = server.getbpub
+
+client.setkey(bpub)
+server.setkey(apub)
+
+clientkey =  client.gethmac
+login = server.checklogin(clientkey)
+puts login
 
